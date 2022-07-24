0882ad66fe104799e4be3fc096f2f9ac43b79cd3
mykenjoro <mykenjoroge01@gmail.com>
2022-07-24 19:53:33 +0300

    Added loan and milk reports

--- app/screens/reports/LoansReport.jsx
+++ app/screens/reports/LoansReport.jsx
@@ -1,12 +1,124 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Title, useTheme } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { collection, getDocs } from "firebase/firestore";
+import { useUserAuth } from "../../context/UserAutContext";
+import { db } from "../../config/firebase";
+import { PieChart } from "react-native-chart-kit";
 
 const LoansReport = () => {
-  return (
-    <View>
-      <Text>LoansReport</Text>
-    </View>
-  )
-}
+  const { colors } = useTheme();
+  const navigation = useNavigation();
+  const [loans, setLoans] = useState([]);
+  const loansRef = collection(db, "loans");
+  const [loading, setLoading] = useState(false);
+  const { user } = useUserAuth();
 
-export default LoansReport
+  const fetchloans = async () => {
+    setLoading;
+    try {
+      setLoading(true);
+      const c = await getDocs(loansRef);
+      setLoans(
+        c.docs
+          .map((doc) => ({ ...doc.data(), id: doc.id }))
+          .filter((c) => c.user.uid === user.uid)
+      );
+      setLoading(false);
+    } catch (error) {
+      alert(error.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchloans();
+  }, []);
+
+  const paidLoansTaken = loans
+    .filter((c) => c.isPaid === true)
+
+    .reduce((acc, curr) => acc + curr.amount, 0);
+
+  const unpaidLoansTaken = loans
+    .filter((c) => c.isPaid === false)
+    .reduce((acc, curr) => acc + curr.amount, 0);
+  console.log(unpaidLoansTaken);
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: colors.background,
+      }}
+    >
+      <ScrollView>
+        <View style={{ flex: 1 }}>
+          <Title
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Loan Performance report
+          </Title>
+          <PieChart
+            data={[
+              {
+                name: "Paid Loans",
+                population: paidLoansTaken,
+                color: "rgba(131, 167, 234, 1)",
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15,
+              },
+              {
+                name: "Unpaid Loans",
+                population: unpaidLoansTaken,
+                color: "#F00",
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15,
+              },
+            ]}
+            width={Dimensions.get("window").width - 16}
+            height={220}
+            chartConfig={{
+              backgroundColor: "#1cc910",
+              backgroundGradientFrom: "#eff3ff",
+              backgroundGradientTo: "#efefef",
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+            }}
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+            }}
+            accessor="population"
+            backgroundColor="transparent"
+            paddingLeft="15"
+            absolute //for the absolute number remove if you want percentage
+          />
+          <Text
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Paid Loans Taken: {paidLoansTaken}
+          </Text>
+          <Text
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Unpaind Loans Taken: {unpaidLoansTaken}
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default LoansReport;
--- app/screens/reports/MilkReport.jsx
+++ app/screens/reports/MilkReport.jsx
@@ -1,12 +1,141 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Title, useTheme } from "react-native-paper";
+import { useUserAuth } from "../../context/UserAutContext";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import moment from "moment";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { BarChart } from "react-native-chart-kit";
 
 const MilkReport = () => {
-  return (
-    <View>
-      <Text>MilkReport</Text>
-    </View>
-  )
-}
+  const { colors } = useTheme();
+  const [milk, setMilk] = React.useState([]);
+  const milkRef = collection(db, "milk");
+  const [loading, setLoading] = useState(false);
+  const { user } = useUserAuth();
 
-export default MilkReport
+  const fetchMilk = async () => {
+    setLoading;
+    try {
+      const c = await getDocs(milkRef);
+      setMilk(
+        c.docs
+          .map((doc) => ({ ...doc.data(), id: doc.id }))
+          .filter((c) => c.user.uid === user.uid)
+      );
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchMilk();
+  }, []);
+
+  //totalMilk produced AmTotal, PmTotal, NoonTotal
+  const totalMilk = milk.reduce(
+    (acc, curr) => {
+      return {
+        AmTotal: parseInt(acc.AmTotal) + parseInt(curr.AmTotal),
+        PmTotal: parseInt(acc.PmTotal) + parseInt(curr.PmTotal),
+        NoonTotal: parseInt(acc.NoonTotal) + parseInt(curr.NoonTotal),
+      };
+    },
+    { AmTotal: 0, PmTotal: 0, NoonTotal: 0 }
+  );
+
+  // total ProducedPerCowToday
+  const totalProducedPerCow = milk.reduce((acc, curr) => {
+    return {
+      ...acc,
+      [curr.cow.name]: curr.totalProduced,
+    };
+  }, {});
+
+  // convert to array
+  const totalProducedPerCowArray = Object.keys(totalProducedPerCow).map(
+    (key) => {
+      return {
+        name: key,
+
+        totalProduced: totalProducedPerCow[key],
+      };
+    }
+  );
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+      <Title
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Morning, Afternoon, and Night Milk production
+      </Title>
+      <BarChart
+        data={{
+          labels: ["AM", "PM", "Noon"],
+          datasets: [
+            {
+              data: [totalMilk.AmTotal, totalMilk.PmTotal, totalMilk.NoonTotal],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width - 16}
+        height={220}
+        yAxisLabel={"Rs"}
+        chartConfig={{
+          backgroundColor: "#1cc910",
+          backgroundGradientFrom: "#eff3ff",
+          backgroundGradientTo: "#efefef",
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+      <Title
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Milk Production per Cow
+      </Title>
+      <BarChart
+        data={{
+          labels: [...totalProducedPerCowArray.map((c) => c.name)],
+          datasets: [
+            {
+              data: [...totalProducedPerCowArray.map((c) => c.totalProduced)],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width - 16}
+        height={220}
+        yAxisLabel={"Rs"}
+        chartConfig={{
+          backgroundColor: "#1cc910",
+          backgroundGradientFrom: "#eff3ff",
+          backgroundGradientTo: "#efefef",
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default MilkReport;
